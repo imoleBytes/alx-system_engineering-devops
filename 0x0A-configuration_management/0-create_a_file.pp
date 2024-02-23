12165246a7b7c159e81ab95c9f11c25403ea4564
imoleFolio <dremkay71@gmail.com>
2024-02-23 14:20:26 +0100

    task0 ples

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
 # Using Puppet, create a file in /tmp.
 
-$filepath = '/tmp/school'
-
 file { '/tmp/school':
-    content => 'I love Puppet'
-    mode    => '0744'
-    owner   => 'www-data'
-    group   => 'www-data'
+  ensure  => file,
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
 }
