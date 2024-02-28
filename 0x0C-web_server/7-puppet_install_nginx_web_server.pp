3963a9a69c2f4fc8c187ba07a0907105e48527e6
imoleFolio <dremkay71@gmail.com>
2024-02-28 15:36:17 +0100

    task7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,21 @@
+# Installs a Nginx server
+
+exec {'install':
+  command  => 'sudo apt update ; sudo apt -y install nginx',
+  provider => shell,
+}
+
+exec {'Hello':
+  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation
+\/redirect_me {\\n\\t\\treturn 301 https:\/\/x.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+  provider => shell,
+}
+
+exec {'run':
+  command  => 'sudo service nginx restart',
+  provider => shell,
+}
