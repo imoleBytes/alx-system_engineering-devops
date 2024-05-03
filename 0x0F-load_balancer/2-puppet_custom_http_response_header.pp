8bb5cf7a527d5f46d7f015fed109e4dc9c744afd
imoleFolio <dremkay71@gmail.com>
2024-05-03 12:16:01 +0100

    2-puppet to automate add_header, restructured

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,6 @@
 # Installs a Nginx server
 
-exec {'install nginx':
+exec {'install':
   command  => 'sudo apt update ; sudo apt -y install nginx',
   provider => shell,
 }
@@ -10,16 +10,10 @@ exec {'Hello':
   provider => shell,
 }
 
-exec {'404':
-  command  => 'sudo echo "Ceci n'est pas une page" | sudo tee /var/www/html/404.html',
-  provider => shell,
-}
 
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\n\tlocation \/redirect_me {\n\t\treturn 301 https:\/\/x.com\/;\n\t}/g" /etc/nginx/sites-available/default':
-  provider => shell,
-}
 
-exec {'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";\n/" /etc/nginx/nginx.conf':
+exec {'add_header':
+  command => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";\n/" /etc/nginx/nginx.conf',
   provider => shell,
 }
 
