ada387f33e4aa6ad61faa4441f0d66d5b5bf09cb
imoleFolio <dremkay71@gmail.com>
2024-05-03 12:10:10 +0100

    2-puppet to automate add_header, sudo added where necessary

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,29 +1,29 @@
 # Installs a Nginx server
 
 exec {'install nginx':
-  command  => 'apt update ; apt -y install nginx',
+  command  => 'sudo apt update ; sudo apt -y install nginx',
   provider => shell,
 }
 
 exec {'Hello':
-  command  => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+  command  => 'sudo echo "Hello World!" | sudo tee /var/www/html/index.html',
   provider => shell,
 }
 
 exec {'404':
-  command  => 'echo "Ceci n'est pas une page" | tee /var/www/html/404.html',
+  command  => 'sudo echo "Ceci n'est pas une page" | sudo tee /var/www/html/404.html',
   provider => shell,
 }
 
-exec {'sed -i "s/listen 80 default_server;/listen 80 default_server;\n\tlocation \/redirect_me {\n\t\treturn 301 https:\/\/x.com\/;\n\t}/g" /etc/nginx/sites-available/default':
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\n\tlocation \/redirect_me {\n\t\treturn 301 https:\/\/x.com\/;\n\t}/g" /etc/nginx/sites-available/default':
   provider => shell,
 }
 
-exec {'sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";\n/" /etc/nginx/nginx.conf':
+exec {'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";\n/" /etc/nginx/nginx.conf':
   provider => shell,
 }
 
 exec {'run':
-  command  => 'service nginx restart',
+  command  => 'sudo service nginx restart',
   provider => shell,
 }
