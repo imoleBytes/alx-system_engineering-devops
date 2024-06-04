b03a91b4c74c23a40601152554a8d2086b4caecc
imoleFolio <dremkay71@gmail.com>
2024-06-04 12:34:04 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+""" Module contain number_of_subscribers function that
+queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0.
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """returns number of subs in a subreddit"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        # "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "0x16.api.advanced:v1.0.0 (by /u/imole)"
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    results = requests.get(url=url, allow_redirects=False, headers=headers)
+    if results.status_code == 404:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    return (results.json().get("data").get('subscribers'))
