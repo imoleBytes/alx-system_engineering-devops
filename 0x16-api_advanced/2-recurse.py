23c43d08d685dea6cbc7c938dbaf3603b8e59f9e
imoleFolio <dremkay71@gmail.com>
2024-06-04 14:41:40 +0100

    task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -40,14 +40,14 @@ def recurse(subreddit, hot_list: list = [], count=0, after=""):
     return hot_list
 
 
-if __name__ == '__main__':
-    import sys
-    recurse = __import__('2-recurse').recurse
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        result = recurse(sys.argv[1])
-        if result is not None:
-            print(len(result))
-        else:
-            print("None")
+# if __name__ == '__main__':
+#     import sys
+#     recurse = __import__('2-recurse').recurse
+#     if len(sys.argv) < 2:
+#         print("Please pass an argument for the subreddit to search.")
+#     else:
+#         result = recurse(sys.argv[1])
+#         if result is not None:
+#             print(len(result))
+#         else:
+#             print("None")
